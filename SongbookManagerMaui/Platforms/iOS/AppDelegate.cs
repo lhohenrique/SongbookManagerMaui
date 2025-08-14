bec50fb1d5348599ebe71e8fc0fc721be1a119e6
SAMSUNGPROJECT\lho <lho@cin.ufpe.br>
2025-08-14 15:16:44 -0300

    Create SongbookManagerMaui project

---
+++ SongbookManagerMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SongbookManagerMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
